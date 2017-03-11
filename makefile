c88e934e2aa67712144121231c2291f1ac270ec7
tisseau <tisseau@enib.fr>
2017-03-11 18:48:33 +0100

    Revert "premier essai"
    This reverts commit 30334fb58fb422c7ddb33767abed00fed163dcfd.

--- makefile
+++
@@ -1,9 +0,0 @@
-TCS=tsc
-NODE=node
-BUILDDIR=site
-
-enibook.js: tsconfig.json src/document.ts src/tools.ts
-	${TCS}
-
-clean:
-	rm -f ${BUILDIR}/enibook.js
--- src/typings/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="globals/ace/index.d.ts" />
-/// <reference path="globals/jquery/index.d.ts" />
-/// <reference path="globals/requirejs/index.d.ts" />
-/// <reference path="modules/markdown-it/index.d.ts" />
